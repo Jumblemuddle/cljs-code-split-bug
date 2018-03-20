06b9116a9d7b48b2d02513ea566c2d9257b78136
Garrett Hopper <garrett@garretthopper.com>
2018-03-20 16:41:07 -0500

    build.edn, deps.edn

--- build.clj
+++ build.clj
@@ -1,10 +1,13 @@
 (require '[cljs.build.api :as b])
 
 (b/build "src" {:output-dir "out"
+                :output-to "out/main.js"
                 :asset-path "/out"
                 :modules {:a {:entries '#{code.split.a}
                               :output-to "out/a.js"}
                           :b {:entries '#{code.split.b}
                               :output-to "out/b.js"}
                           :c {:entries '#{code.split.c}
-                              :output-to "out/c.js"}}})
+                              :output-to "out/c.js"}}
+                :verbose true
+                :aot-cache true})
